5fc460529ea12e1f61d9da0e7129f75ee1113ee3
Mariana Pantelic <mari27line@yahoo.com>
2021-05-12 16:03:24 +0200

    Added first 2 slides

--- Frontend/blog/src/pages/home.js
+++ Frontend/blog/src/pages/home.js
@@ -1,33 +1,31 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import img1 from "../img/woman-570883_1920.jpg";
+import img2 from "../img/ocean-1867285_1920.jpg";
 
 const Home = () => {
   return (
     <>
       <Carousel>
-        <Carousel.Item interval={5000}>
+        <Carousel.Item interval={2000}>
           <img className="d-block w-100" src={img1} alt="First slide" />
           <Carousel.Caption>
             <h2 className="happy-title">What makes you happy?</h2>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
+        <Carousel.Item interval={2000}>
+          <img className="d-block w-100" src={img2} alt="Second slide" />
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Carousel.Caption className="blog-title">
+            <h2>This is a blog</h2>
+            <h2>about the little things</h2>
+            <h2>that make me happy!</h2>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={5000}>
+        <Carousel.Item interval={2000}>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            src={"holder.js/800x400?text=Third slide&bg=20232a"}
             alt="Third slide"
           />
 
