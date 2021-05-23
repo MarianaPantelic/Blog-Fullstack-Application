0cbca330709bdfbecf02e445229c436004c44e1e
Mariana Pantelic <mari27line@yahoo.com>
2021-05-23 19:49:25 +0200

    Added dependencies, cors and error handling

--- Backend/blog/app.js
+++ Backend/blog/app.js
@@ -1,20 +1,45 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const blogRouter = require("./routes/blog");
+
+const { setCors } = require("./middleware/security");
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
 var app = express();
 
-app.use(logger('dev'));
+const adapter = new FileSync("data/db.json");
+const db = low(adapter);
+//add default entries to the DATABASE
+db.defaults({
+  posts: [],
+}).write();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/blog", blogRouter);
+
+app.use((err, req, res, next) => {
+  //respond to the requestor with the error messages
+  //set response status to 500
+  console.log(err);
+  res.status(500).send({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 module.exports = app;
