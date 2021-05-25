a4b71ae23d91491fbd1eaed4867e91866f9e851d
Mariana Pantelic <mari27line@yahoo.com>
2021-05-25 23:56:03 +0200

    Added styles

--- Frontend/blog/src/pages/post.js
+++ Frontend/blog/src/pages/post.js
@@ -9,34 +9,34 @@ import { useForm } from "react-hook-form";
 
 const Post = () => {
   const { quill, quillRef } = useQuill();
-  const { register, handleSubmit } = useForm();
+  const [state, setState] = useState({ user: "", title: "", content: "" });
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
   return (
     <>
       <Container>
         <div className="post-container">
-          <h2>What made you smile today?</h2>
+          <h2 className="mt-5">What made you smile today?</h2>
+          <Form.Control
+            size="lg"
+            type="text"
+            placeholder="Enter your name"
+            className="title-input mx-auto mt-5"
+          />
           <Form.Control
             size="lg"
             type="text"
             placeholder="Enter your title"
-            className="title-input mx-auto"
+            className="title-input mx-auto mt-5"
           />
           <div className="quill mx-auto">
             <div ref={quillRef} />
           </div>
+          <Link to={"/"}>
+            <button>Back to home</button>
+          </Link>
+          <button>Post</button>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input ref={register} type="file" name="picture" />
-          <button>Submit</button>
-        </form>
       </Container>
-      <Link to={"/"}>
-        <button>Back to home</button>
-      </Link>
     </>
   );
 };
