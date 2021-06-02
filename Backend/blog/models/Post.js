f4dcee16f3b6fa39e94c988602b8cf16d13ec1ae
Mariana Pantelic <mari27line@yahoo.com>
2021-06-02 13:51:01 +0200

    Added the model PostSchema

---
+++ Backend/blog/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    video: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    toObject: {
+      virtuals: true,
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
