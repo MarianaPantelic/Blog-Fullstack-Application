cd7ea5f69b36304eca5b7868395b4f2da77f65a7
Mariana Pantelic <mari27line@yahoo.com>
2021-06-16 13:32:35 +0200

    Added virtuals to the post schema

--- Backend/blog/models/Post.js
+++ Backend/blog/models/Post.js
@@ -1,39 +1,49 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const PostSchema = new Schema({
-  user: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: Object,
-    required: true,
-    ops: {
-      type: Array,
-      insert: {
-        type: String,
-        required: true,
-      },
-      insert: {
-        type: Object,
-        image: {
-          data: Buffer,
-          contentType: String,
+const PostSchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: Object,
+      required: true,
+      ops: {
+        type: Array,
+        insert: {
+          type: String,
+          required: true,
+        },
+        insert: {
+          type: Object,
+          image: {
+            data: Buffer,
+            contentType: String,
+          },
         },
       },
     },
+    clicked: {
+      type: Boolean,
+    },
+    likes: {
+      type: Number,
+    },
   },
-  clicked: {
-    type: Boolean,
-  },
-  likes: {
-    type: Number,
-  },
-});
+  {
+    toObject: {
+      virtuals: true,
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 module.exports = mongoose.model("Post", PostSchema);
