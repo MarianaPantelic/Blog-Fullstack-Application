99c3a4118e23656f1e6f2382e08c63b68a4775c4
Mariana Pantelic <mari27line@yahoo.com>
2021-06-22 14:43:27 +0200

    Added register page

--- Frontend/blog/src/pages/login.js
+++ Frontend/blog/src/pages/login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Login = () => {
--- Frontend/blog/src/pages/register.js
+++ Frontend/blog/src/pages/register.js
@@ -1,32 +1,30 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 
 const Register = () => {
   return (
-    <Container>
+    <Container className="register-container">
+      <h3 className="text-center pacifico-font">Create an account</h3>
       <Form>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>User Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your username" />
+          <Form.Label className="m-3">User Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your user name" />
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label className="m-3">Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label className="m-3">Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter your Password" />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+
+        <div className="text-center mt-5">
+          <button type="submit" className="pacifico-font">
+            Register
+          </button>
+        </div>
       </Form>
     </Container>
   );
